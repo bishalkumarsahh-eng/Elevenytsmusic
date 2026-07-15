31cbfb861091d20087f70347baf728500ac93350
bishalkumarsahh-eng <bishalkumarsahh@gmail.com>
2026-07-16 01:56:52 +0530

    Update _emoji.py

--- Elevenyts/helpers/_emoji.py
+++ Elevenyts/helpers/_emoji.py
@@ -1,17 +1,17 @@
 # ==========================================================
-  # Copyright (c) 2026 ArtistBots
-  # All Rights Reserved.
-  #
-  # Project      : ArtistBots API Telegram Music Bot
-  # Powered By   : Artist
-  # Type         : API Based Telegram Music Bot
-  #
-  # Bot          : @ArtistApibot
-  # Channel      : https://t.me/artistbots
-  # GitHub       : https://github.com/elevenyts
-  #
-  # Unauthorized copying, modification, or redistribution
-  # of this source code without permission is prohibited.
-  # ==========================================================
+# Copyright (c) 2026 VelocityBots 
+# All Rights Reserved.
+#
+# Project      : VelocityBots API Telegram Music Bot
+# Powered By   : VelocityBots 
+# Type         : API Based Telegram Music Bot
+#
+# Bot          : @JunoXmusic_Robot
+# Channel      : https://t.me/junoxmusic_updates
+# GitHub       : https://github.com/bishalkumarsahh-eng
+#
+# Unauthorized copying, modification, or redistribution
+# of this source code without permission is prohibited.
+# ==========================================================
 import base64
 exec(base64.b64decode("IyA9PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09CiMgQ29weXJpZ2h0IChjKSAyMDI2IEFydGlzdEJvdHMKIyBBbGwgUmlnaHRzIFJlc2VydmVkLgojCiMgUHJvamVjdCAgICAgIDogQXJ0aXN0Qm90cyBBUEkgVGVsZWdyYW0gTXVzaWMgQm90CiMgUG93ZXJlZCBCeSAgIDogQXJ0aXN0CiMgVHlwZSAgICAgICAgIDogQVBJIEJhc2VkIFRlbGVncmFtIE11c2ljIEJvdAojCiMgQm90ICAgICAgICAgIDogQEFydGlzdEFwaWJvdAojIENoYW5uZWwgICAgICA6IGh0dHBzOi8vdC5tZS9hcnRpc3Rib3RzCiMgR2l0SHViICAgICAgIDogaHR0cHM6Ly9naXRodWIuY29tL2VsZXZlbnl0cwojCiMgVW5hdXRob3JpemVkIGNvcHlpbmcsIG1vZGlmaWNhdGlvbiwgb3IgcmVkaXN0cmlidXRpb24KIyBvZiB0aGlzIHNvdXJjZSBjb2RlIHdpdGhvdXQgcGVybWlzc2lvbiBpcyBwcm9oaWJpdGVkLgojID09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT0KCmltcG9ydCBpdGVydG9vbHMKCiMgUm90YXRpbmcgZW1vamkgc2VxdWVuY2Ugc2hvd24gb24gL3BsYXkgYW5kIG9uIGF1dG9wbGF5IG1lc3NhZ2VzLgojIEVhY2ggY2FsbCB0byBuZXh0X3BsYXlfZW1vamkoKSBhZHZhbmNlcyB0byB0aGUgbmV4dCBlbW9qaSBpbiB0aGUgbGlzdCwKIyB3cmFwcGluZyBiYWNrIHRvIHRoZSBzdGFydCBvbmNlIHRoZSBlbmQgaXMgcmVhY2hlZCDigJQgc28gY29uc2VjdXRpdmUKIyAvcGxheSAoYW5kIGF1dG9wbGF5KSB0cmlnZ2VycyBlYWNoIHNob3cgYSBkaWZmZXJlbnQgZW1vamkgaW4gdHVybi4KUExBWV9FTU9KSVMgPSBbIvCfkp4iLCAi4pyoIiwgIvCfjrYiLCAi8J+MnyIsICLwn5SlIiwgIvCfjqciLCAi8J+MiCIsICLwn5KrIl0KCl9lbW9qaV9jeWNsZSA9IGl0ZXJ0b29scy5jeWNsZShQTEFZX0VNT0pJUykKCgpkZWYgbmV4dF9wbGF5X2Vtb2ppKCkgLT4gc3RyOgogICAgcmV0dXJuIG5leHQoX2Vtb2ppX2N5Y2xlKQo=").decode("utf-8"))
