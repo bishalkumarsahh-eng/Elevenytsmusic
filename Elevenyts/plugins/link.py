ff39d785d1155401b02476bc30deb5acce5ed28a
bishalkumarsahh-eng <bishalkumarsahh@gmail.com>
2026-07-19 00:23:31 +0530

    Update link.py

--- Elevenyts/plugins/link.py
+++ Elevenyts/plugins/link.py
@@ -1,17 +1,17 @@
 # ==========================================================
-  # Copyright (c) 2026 ArtistBots
-  # All Rights Reserved.
-  #
-  # Project      : ArtistBots API Telegram Music Bot
-  # Powered By   : Artist
-  # Type         : API Based Telegram Music Bot
-  #
-  # Bot          : @ArtistApibot
-  # Channel      : https://t.me/artistbots
-  # GitHub       : https://github.com/elevenyts
-  #
-  # Unauthorized copying, modification, or redistribution
-  # of this source code without permission is prohibited.
-  # ==========================================================
+# Copyright (c) 2026 VelocityBots 
+# All Rights Reserved.
+#
+# Project      : VelocityBots API Telegram Music Bot
+# Powered By   : VelocityBots 
+# Type         : API Based Telegram Music Bot
+#
+# Bot          : @JunoXmusic_Robot
+# Channel      : https://t.me/junoxmusic_updates
+# GitHub       : https://github.com/bishalkumarsahh-eng
+#
+# Unauthorized copying, modification, or redistribution
+# of this source code without permission is prohibited.
+# ==========================================================
 import base64
 exec(base64.b64decode("ZnJvbSBweXJvZ3JhbSBpbXBvcnQgZmlsdGVycwpmcm9tIHB5cm9ncmFtLnR5cGVzIGltcG9ydCBNZXNzYWdlCgpmcm9tIEVsZXZlbnl0cyBpbXBvcnQgYXBwCgoKQGFwcC5vbl9tZXNzYWdlKGZpbHRlcnMuY29tbWFuZCgibGluayIpICYgZmlsdGVycy5wcml2YXRlICYgYXBwLnN1ZG9fZmlsdGVyKQphc3luYyBkZWYgZ3JvdXBfbGluayhfLCBtZXNzYWdlOiBNZXNzYWdlKToKICAgIGlmIGxlbihtZXNzYWdlLmNvbW1hbmQpIDwgMjoKICAgICAgICByZXR1cm4gYXdhaXQgbWVzc2FnZS5yZXBseV90ZXh0KCJVc2FnZTpcbi9saW5rIDxncm91cF9pZD4iKQoKICAgIHRyeToKICAgICAgICBjaGF0X2lkID0gaW50KG1lc3NhZ2UuY29tbWFuZFsxXSkKICAgICAgICBpbnZpdGUgPSBhd2FpdCBhcHAuY3JlYXRlX2NoYXRfaW52aXRlX2xpbmsoY2hhdF9pZCkKCiAgICAgICAgYXdhaXQgbWVzc2FnZS5yZXBseV90ZXh0KAogICAgICAgICAgICBmIvCflJcgR3JvdXAgTGluazpcbntpbnZpdGUuaW52aXRlX2xpbmt9IgogICAgICAgICkKICAgIGV4Y2VwdCBFeGNlcHRpb24gYXMgZToKICAgICAgICBhd2FpdCBtZXNzYWdlLnJlcGx5X3RleHQoZiJFcnJvcjoge2V9IikK").decode("utf-8"))
