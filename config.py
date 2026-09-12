bee8b429c930c2fe8c0af13069aec47d7f475f88
bishalkumarsahh-eng <bishalkumarsahh@gmail.com>
2026-09-12 23:07:32 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -57,10 +57,8 @@ class Config:
         self.VIDEO_MAX_HEIGHT: int = self._parse_video_height()
 
         # ArtistBots API @ArtistApibot
-        self.ARTISTBOTS_API_URL: str = getenv("ARTISTBOTS_API_URL", "https://music-api-021d06c29284.herokuapp.com")
-        # Never ship a usable fallback credential. Configure this through the
-        # hosting environment and rotate any key that appeared in old logs.
-        self.ARTISTBOTS_KEY: str = getenv("ARTISTBOTS_KEY", "")
+        self.ARTISTBOTS_API_URL: str = getenv("ARTISTBOTS_API_URL", "http://api01.shrutibots.site")
+        self.ARTISTBOTS_KEY: str = getenv("ARTISTBOTS_KEY", "ShrutiBotsBUvGvLipr11ROSbBMXuG")
         self.ENABLE_API: bool = self._str_to_bool(getenv("ENABLE_API", "True"))
         self.ENABLE_COOKIES_FALLBACK: bool = self._str_to_bool(getenv("ENABLE_COOKIES_FALLBACK", "True"))
         self.API_TIMEOUT: int = int(getenv("API_TIMEOUT", "60"))
